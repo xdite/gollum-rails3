2779c3df13aed47c4468bfb8b2ac7ec27815dbc1
xdite <xdite@pixnet.tw>
2010-10-19 15:55:57 -0700

    inistal project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GollumRails3::Application.config.secret_token = '96a459860072bebcf5d9e24429852f30a5bd6aa73c034f602d4fa15df6b6ccd3dc6efc6fbd42082f21cdca69eca8009ff2a0b72c7d12f05c200a9fd2af496cd8'
