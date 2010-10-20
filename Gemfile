741a5b4be5fb9c43d5b11050bafef27335060f2f
xdite <xdite@pixnet.tw>
2010-10-19 17:52:17 -0700

    move gollum asset to here

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'rails', '3.0.0'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'mysql2'
+gem 'mustache'
+gem 'mustache_rails3'
 
 group :development, :test do
   gem "rspec-rails", ">= 2.0.0.rc" 
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 GollumRails3::Application.initialize!
+
+GollumRails3::Application.configure do
+  config.load_paths += %W{ #{RAILS_ROOT}/app/mustaches } 
+end
